b38b4b8b6c1130c968c4396fc6b48a59d995b12c
Tony <goofballtech@gmail.com>
2018-01-02 21:17:34 -0600

    Assignment Push

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,91 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    private TextView mMainText;
+    private Button mTopButton;
+    private Button mBottomButton;
 
+    private String mCurrentPage = "T1_Story";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMainText = (TextView) findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentPage.equals("End")){
+                    finish();
+                }else {
+                    nextPage(mCurrentPage, "Ans1");
+                }
+            }
+        });
 
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentPage.equals("End")){
+                    finish();
+                }else {
+                    nextPage(mCurrentPage, "Ans2");
+                }
+            }
+        });
 
+    }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+    private void nextPage(String onPage, String answerChoice){
+       if (onPage.equals("T1_Story")){
+           if (answerChoice.equals("Ans1")){
+               mMainText.setText(R.string.T3_Story);
+               mCurrentPage = "T3_Story";
+               mTopButton.setText(R.string.T3_Ans1);
+               mBottomButton.setText(R.string.T3_Ans2);
+           }else{
+               mMainText.setText(R.string.T2_Story);
+               mCurrentPage = "T2_Story";
+               mTopButton.setText(R.string.T2_Ans1);
+               mBottomButton.setText(R.string.T2_Ans2);
+           }
+       }else if (onPage.equals("T2_Story")) {
+           if (answerChoice.equals("Ans1")) {
+               mMainText.setText(R.string.T3_Story);
+               mCurrentPage = "T3_Story";
+               mTopButton.setText(R.string.T3_Ans1);
+               mBottomButton.setText(R.string.T3_Ans2);
+           } else {
+               mMainText.setText(R.string.T4_End);
+               mCurrentPage = "End";
+               mTopButton.setText("The");
+               mBottomButton.setText("End");
+           }
+       }else if (onPage.equals("T3_Story")){
+           if (answerChoice.equals("Ans1")){
+               mMainText.setText(R.string.T6_End);
+               mCurrentPage = "End";
+               mTopButton.setText("The");
+               mBottomButton.setText("End");
+           }else{
+               mMainText.setText(R.string.T5_End);
+               mCurrentPage = "End";
+               mTopButton.setText("The");
+               mBottomButton.setText("End");
+           }
+       }
     }
 }
